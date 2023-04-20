e208e0ce278c50ff2d6f9ad4ad4fe76a676633e5
kavikannavkl <kavikannavkl@gmail.com>
2023-04-20 22:24:40 +0530

    first commit

---
+++ arraysum.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int i,sum=0;
+	int a[5]={};    //**since array curly braces are given **
+	                // if curly braces are not given then they are used to store strings
+	int length=sizeof(a)/sizeof(0);               
+	                
+	printf("**** Sum of array numbers ****");
+	
+	
+	for(i=0;i<length;i++)
+	
+	{
+		
+		
+	printf("\n enter the number ");
+	scanf("%d",&a[i]);
+	sum=sum+a[i];
+	
+	
+    }
+	printf("the sum of array elemnts are: %d",sum);
+	return 0;
+}
