ed86f86fd76dbd693e7c42307a6f7145b4f9dea1
kavikannavkl <kavikannavkl@gmail.com>
2023-04-21 11:41:51 +0530

    first commit

---
+++ ASCII.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	char a;
+	printf("enter any charracter to know its ASCII value :");
+	scanf("%c",&a);
+	printf("the ASCII value of %c is %d",a,a);
+    return 0;
+}
+
