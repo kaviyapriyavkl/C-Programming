10d61e72ae78cdc5292dc130c83af5b96c8d3253
kavikannavkl <kavikannavkl@gmail.com>
2023-04-30 11:31:46 +0530

    firts commit

---
+++ size.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+    int main(){
+	printf("the size of int :%d",sizeof(int));
+	printf("\nthe size of char :%d",sizeof(char));
+	printf("\nthe size of float :%d",sizeof(float));
+	printf("\nthe size of double :%d",sizeof(double));
+	return 0;
+}
