c1de1df1a8888e6754f82b326bc5eac4aab67029
kavikannavkl <kavikannavkl@gmail.com>
2023-05-03 20:59:05 +0530

    first commit

---
+++ simplesum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("ADDItION");
+	printf("enter the 1st num :");
+	scanf("%d",&a);
+	printf("enetr the 2nd num :");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("sum of %d and %d is :%d",a,b,sum);
+	return 0;
+}
